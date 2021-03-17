605e973123d8481c469a6e03de5a9a466ee0ee67
Jose Eduardo Rodrigues Pinto (jerp86) <jerp4@hotmail.com>
2021-03-16 21:25:28 -0300

    added Icon in drawer routes

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Feather } from '@expo/vector-icons';
 
 import Home from './pages/Home';
 import Search from './pages/Search';
@@ -12,7 +13,8 @@ const Routes = () => (
       name="Home"
       component={Home}
       options={{
-        title: 'Meu Local'
+        title: 'Meu Local',
+        drawerIcon: () => <Feather name="map-pin" size={16} />,
       }}
     />
 
@@ -20,7 +22,8 @@ const Routes = () => (
       name="Search"
       component={Search}
       options={{
-        title: 'Procurar'
+        title: 'Procurar',
+        drawerIcon: () => <Feather name="search" size={16} />
       }}
     />
   </Drawer.Navigator>
