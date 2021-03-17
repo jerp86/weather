c53407e8d81f64fbae3ec297a732724655997bc4
Jose Eduardo Rodrigues Pinto (jerp86) <jerp4@hotmail.com>
2021-03-16 22:32:53 -0300

    added helper Condition in utils

---
+++ src/utils/condition.js
@@ -0,0 +1,39 @@
+export const condition = condition => {
+  switch(condition) {
+    case 'storm':
+      return icon = {
+        name: 'thunderstorm-outline',
+        color: '#1EC9FF',
+      };
+    case 'clear_day':
+      return icon = {
+        name: 'sunny-outline',
+        color: '#FFB300',
+      };
+    case 'clear_night':
+      return icon = {
+        name: 'moon-outline',
+        color: '#1EC9FF',
+      };
+    case 'cloudly_day':
+      return icon = {
+        name: 'partly-sunny-outline',
+        color: '#FFB300',
+      };
+    case 'cloud' | 'cloudly_night':
+      return icon = {
+        name: 'cloudy-night-outline',
+        color: '#1EC9FF',
+      };
+    case 'rain' | 'hail':
+      return icon = {
+        name: 'rainy-outline',
+        color: '#1EC9FF',
+      };
+    default:
+      return icon = {
+        name: 'alert-circle-outline',
+        color: 'red',
+      };
+  }
+};
