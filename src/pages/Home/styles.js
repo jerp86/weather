093995752e39f23ef861d79dac91d762160dfd57
Jose Eduardo Rodrigues Pinto (jerp86) <jerp4@hotmail.com>
2021-03-20 20:29:29 -0300

    added another styles  in Home

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -5,8 +5,18 @@ export const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    padding: '5%',
+    padding: '2%',
+    paddingTop: '5%',
     backgroundColor: '#E8F0FF',
+  },
+  loading: {
+    fontSize: 17,
+    fontStyle: 'italic',
+  },
+  error: {
+    fontSize: 20,
+    color: '#FF0000',
+    textAlign: 'center',
   },
   list: {
     marginTop: 8,
